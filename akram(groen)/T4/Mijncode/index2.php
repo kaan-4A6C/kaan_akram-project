0958ff6f6ddd60d72847557ae6926e446be0ee23
Akram-4A6C <512135@edu.rocmn.nl>
2017-06-23 09:55:39 +0200

    veel

---
+++ akram(groen)/T4/Mijncode/index2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html,>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style2.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+        <title>Over de RHEIN | Akram</title>
+    </head>
+    <body>
+        <div class="top">
+            <div class="logo">Over de <br>RHEIN</div>
+            <div class="title">Kabelchecklisten_3</div>
+            <button class="button" type="button" name="button">Bestand</button>
+            <button class="button" type="button" name="button">Info</button>
+        </div>
+        <div class="content">
+            <?php include 'function2.php'; ?>
+            <form class="" action="index.html" method="post">
+              <fieldset>
+                <legend>Hijskraan</legend>
+                Fabrikaat: <input type="text" name="" value=""><br><br>
+                Model/Type: <input type="text" name="" value=""><br><br>
+                Serienummer: <input type="text" name="" value=""><br><br>
+                Bedrijsnummer: <input type="text" name="" value=""><br><br>
+                Bouwjaar: <input type="text" name="" value=""><br><br>
+              </fieldset>
+            </form>
+
+
+        </div>
+        </div>
+        <button class="exit">exit</button>
+    </body>
+</html>
